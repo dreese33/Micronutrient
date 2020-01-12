780234635be116d6f2eab2ee81c3dae7ed2b7ffd
dreese33 <dylanjacobreese@gatech.edu>
2020-01-11 20:04:38 -0500

    Added setup code

---
+++ Micronutrient Libraries/Micronutrient Libraries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Micronutrient Libraries
+//
+//  Created by Eric Reese on 1/11/20.
+//  Copyright © 2020 ReeseGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
