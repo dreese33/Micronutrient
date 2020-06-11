3ee5f2287d7f5024257448c695dc3a93837df5a2
dreese33 <dylanjacobreese@gatech.edu>
2020-06-11 18:45:37 -0400

    Get current date functionality

--- Micronutrient Libraries/Micronutrient Libraries/MasterMicronutrientController.swift
+++ Micronutrient Libraries/Micronutrient Libraries/MasterMicronutrientController.swift
@@ -15,9 +15,27 @@ final class MasterMicronutrientController: UIViewController {
     @IBAction func saveFoods(_ sender: UIButton) {
         print("Saving")
         
+        //Get current date
+        let currentDate = self.getCurrentDate()
+        
+        
         //Save food here
+        if let food = MicronutrientController.currentFood {
+            
+        } else {
+            print("Error: MicronutrientController.currentFood does not exist")
+        }
         
         print("Saved")
+    }
+    
+    
+    func getCurrentDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .none
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: date)
     }
     
     
